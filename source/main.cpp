d8694917fda01642e655e093aeacb95f23a6d03c
Zordon <65111609+Zordon1337@users.noreply.github.com>
2022-06-30 23:51:19 +0200

    Add files via upload

---
+++ source/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+    cout<<"now farming your hours :D"<<endl;
+    cout<<"if you have problems feel free to submit issue on github ;)"<<endl;
+    Sleep(41345123541351230);
+
+}
