a1c2aef9dccb6263c83bf01593b63f9311c035cd
Zordon <65111609+Zordon1337@users.noreply.github.com>
2022-07-01 10:36:04 +0200

    now the window is hiding in 3 sec
    i'm must updated like 8 game launchers now

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
-#include "windows.h"
+#include <windows.h>
 
 using namespace std;
+
+bool ConsoleVisible = true;
 
 int main()
 {
     cout<<"now farming your hours :D"<<endl;
     cout<<"if you have problems feel free to submit issue on github ;)"<<endl;
+    cout<<"you will must shutdown the game from steam"<<endl;
+    cout<<"App will hide in 3 sec"<<endl;
+    Sleep(3000);
+    ShowWindow(GetForegroundWindow(),SW_HIDE);
     Sleep(999999999999999999999999999999999);
 
 }
