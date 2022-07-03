4b038b265ccb96c5d06c9174005d8c93bd81fb7b
Zordon <65111609+Zordon1337@users.noreply.github.com>
2022-07-03 17:04:25 +0200

    i'm literally tired

--- Patcher/Patch.cpp
+++ Patcher/Patch.cpp
@@ -2,16 +2,21 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
+#include <Urlmon.h>
+#include <filesystem>
+#include <fstream>
 
 
 std::string cmd;
+std::string gm;
+std::string dir;
 
 
 
 
 int main()
 {
-    system("color 5a");
+    system("color 90");
     std::cout << "hint: if you don't how to use. use command 'help'" << std::endl;
     std::cout << ">>";
     std::cin >> cmd;
@@ -24,12 +29,17 @@ int main()
         std::cout << "Counter-Strike: Source" << std::endl;
         std::cout << "Muck" << std::endl;
         std::cout << "metin2" << std::endl;
-        std::cout << "Plants vs. Zombies: Game of the Year" << std::endl;
+        std::cout << "PvZ" << std::endl;
         std::cout << "Geometry Dash" << std::endl;
         std::cout << "timberman" << std::endl;
         std::cout << "Dude Simulator 3" << std::endl;
         std::cout << "ProgressBar95" << std::endl;
         std::cout << "Teardown" << std::endl;
+        std::cout << "  " << std::endl;
+        std::cout << "  " << std::endl;
+        std::cout << "Comming soon" << std::endl;
+        std::cout << "  " << std::endl;
+        std::cout << "  " << std::endl;
         std::cout << "Counter-Strike 1.6" << std::endl;
         std::cout << "Half-life" << std::endl;
         std::cout << "Counter-strike: Condition Zero" << std::endl;
@@ -53,10 +63,167 @@ int main()
     }
     if (cmd == "install")
     {
-        std::cout << "Comming soon" << std::endl;
-        system("PAUSE");
-        system("cls");
-        return main();
+        std::cout << "enter game name that you want farm: ";
+        std::cin >> gm;
+
+        /* //////////////////////////// */
+        /*                              */
+        /*    Counter Strike: Source    */
+        /*                              */
+        /* //////////////////////////// */
+        if (gm == "Counter-Strike: Source")
+        {
+            std::cout << "enter game directory(ex. C:/steam/css";
+            std::cin >> dir;
+            std::ifstream f1("./bin/hl2.exe", std::ios::binary);
+            std::ofstream f2(dir + "/hl2.exe", std::ios::binary);
+            f2 << f1.rdbuf();
+            system("cls");
+            std::cout << "Done!" << std::endl;
+            Sleep(1);
+            return main();
+
+        }
+
+        /* //////////////////////////// */
+        /*                              */
+        /*             Muck             */
+        /*                              */
+        /* //////////////////////////// */
+        if (gm == "Muck")
+        {
+            std::cout << "enter game directory (ex. C:/steam/Muck";
+            std::cin >> dir;
+            std::ifstream f1("./bin/Muck.exe", std::ios::binary);
+            std::ofstream f2(dir + "/Muck.exe", std::ios::binary);
+            f2 << f1.rdbuf();
+            system("cls");
+            std::cout << "Done!" << std::endl;
+            Sleep(1);
+            return main();
+        }
+
+        /* //////////////////////////// */
+        /*                              */
+        /*           Metin2             */
+        /*                              */
+        /* //////////////////////////// */
+        if (gm == "metin2")
+        {
+            std::cout << "enter game directory (ex. C:/steam/Metin2";
+            std::cin >> dir;
+            std::ifstream f1("./bin/steam_launcher.exe", std::ios::binary);
+            std::ofstream f2(dir +  "/steam_launcher.exe", std::ios::binary);
+            f2 << f1.rdbuf();
+            system("cls");
+            std::cout << "Done!" << std::endl;
+            Sleep(1);
+            return main();
+        }
+        /* //////////////////////////// */
+        /*                              */
+        /*             PvZ              */
+        /*                              */
+        /* //////////////////////////// */
+        if (gm == "PvZ")
+        {
+            std::cout << "enter game directory(ex. C:/steam/Plants Vs Zombies";
+            std::cin >> dir;
+            std::ifstream f1("./bin/PlantsVsZombies.exe", std::ios::binary);
+            std::ofstream f2(dir + "/PlantsVsZombies.exe", std::ios::binary);
+            f2 << f1.rdbuf();
+            system("cls");
+            std::cout << "Done!" << std::endl;
+            Sleep(1);
+            return main();
+
+        }
+        /* //////////////////////////// */
+        /*                              */
+        /*        Geometry Dash         */
+        /*                              */
+        /* //////////////////////////// */
+        if (gm == "Geometry Dash")
+        {
+            std::cout << "enter game directory (ex. C:/steam/Geometry Dash";
+            std::cin >> dir;
+            std::ifstream f1("./bin/GeometryDash.exe", std::ios::binary);
+            std::ofstream f2(dir + "/GeometryDash.exe", std::ios::binary);
+            f2 << f1.rdbuf();
+            system("cls");
+            std::cout << "Done!" << std::endl;
+            Sleep(1);
+            return main();
+        }
+        /* //////////////////////////// */
+        /*                              */
+        /*           Timberman          */
+        /*                              */
+        /* //////////////////////////// */
+        if (gm == "timberman")
+        {
+            std::cout << "enter game directory (ex. C:/steam/Timberman";
+            std::cin >> dir;
+            std::ifstream f1("./bin/Timberman.exe", std::ios::binary);
+            std::ofstream f2(dir + "/Timberman.exe", std::ios::binary);
+            f2 << f1.rdbuf();
+            system("cls");
+            std::cout << "Done!" << std::endl;
+            Sleep(1);
+            return main();
+        }
+        /* //////////////////////////// */
+        /*                              */
+        /*        Dude Simulator 3      */
+        /*                              */
+        /* //////////////////////////// */
+        if (gm == "PvZ")
+        {
+            std::cout << "enter game directory(ex. C:/steam/Dude Simulator 3";
+            std::cin >> dir;
+            std::ifstream f1("./bin/DudeSimulator3.exe", std::ios::binary);
+            std::ofstream f2(dir + "/DudeSimulator3.exe", std::ios::binary);
+            f2 << f1.rdbuf();
+            system("cls");
+            std::cout << "Done!" << std::endl;
+            Sleep(1);
+            return main();
+
+        }
+        /* //////////////////////////// */
+        /*                              */
+        /*        ProgressBar95         */
+        /*                              */
+        /* //////////////////////////// */
+        if (gm == "Geometry Dash")
+        {
+            std::cout << "enter game directory (ex. C:/steam/Progressbar95";
+            std::cin >> dir;
+            std::ifstream f1("./bin/Progressbar95.exe", std::ios::binary);
+            std::ofstream f2(dir + "/Progressbar95.exe", std::ios::binary);
+            f2 << f1.rdbuf();
+            system("cls");
+            std::cout << "Done!" << std::endl;
+            Sleep(1);
+            return main();
+        }
+        /* //////////////////////////// */
+        /*                              */
+        /*            Teardown          */
+        /*                              */
+        /* //////////////////////////// */
+        if (gm == "timberman")
+        {
+            std::cout << "enter game directory (ex. C:/steam/Teardown";
+            std::cin >> dir;
+            std::ifstream f1("./bin/teardown.exe", std::ios::binary);
+            std::ofstream f2(dir + "/teardown.exe", std::ios::binary);
+            f2 << f1.rdbuf();
+            system("cls");
+            std::cout << "Done!" << std::endl;
+            Sleep(1);
+            return main();
+        }
     }
 }
 
