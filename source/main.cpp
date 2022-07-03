d7022c866e6356a4dc0834080ac2080171c5ab2d
Zordon <65111609+Zordon1337@users.noreply.github.com>
2022-07-03 17:08:23 +0200

    changed time
    Good news: app now instantly hides
    bad news: i must update +15 prenamed :cry:

--- source/main.cpp
+++ source/main.cpp
@@ -10,8 +10,8 @@ int main()
     cout<<"now farming your hours :D"<<endl;
     cout<<"if you have problems feel free to submit issue on github ;)"<<endl;
     cout<<"you will must shutdown the game from steam"<<endl;
-    cout<<"App will hide in 3 sec"<<endl;
-    Sleep(3000);
+    cout<<"App will hide in 1 milisec"<<endl;
+    Sleep(1);
     ShowWindow(GetForegroundWindow(),SW_HIDE);
     Sleep(999999999999999999999999999999999);
 
