71324a740486f22937bc7d08287d3e7ee907db9a
Zordon <65111609+Zordon1337@users.noreply.github.com>
2022-11-07 13:28:28 +0100

    removed useless bool

--- source/main.cpp
+++ source/main.cpp
@@ -3,13 +3,13 @@
 
 using namespace std;
 
-bool ConsoleVisible = true;
+
 
 int main()
 {
     cout<<"now farming your hours :D"<<endl;
     cout<<"if you have problems feel free to submit issue on github ;)"<<endl;
-    cout<<"you will must shutdown the game from steam"<<endl;
+    cout<<"if you want to stop farming. stop the game on steam"<<endl;
     cout<<"App will hide in 1 milisec"<<endl;
     Sleep(1);
     ShowWindow(GetForegroundWindow(),SW_HIDE);
